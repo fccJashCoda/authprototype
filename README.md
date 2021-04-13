ad6c5b8702a8c7f97b8e2b8aa9e85b345656dd03
fccJashCoda <personalrevolution2014@gmail.com>
2021-04-13 21:36:44 +0200

    setting user context to null after loging out, added some comments

---
+++ README.md
@@ -0,0 +1,3 @@
+just some boiler to set auth without using localstorage for jwt tokens
+
+https://levelup.gitconnected.com/react-template-for-jwt-authentication-with-private-routes-and-redirects-f77c488bfb85
--- server/src/api/view/view.controller.js
+++ server/src/api/view/view.controller.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../../models/users');
 
+// check if a cookie is stored in the browser, if there is, try and get the user from the database and
+// send it back to the front end
+// NOTE: yes, the secret key is exposed. This is not production code!, just a little demo to check how to
+// use httponly cookies and play with react context
+// also, we're sending the password to the frontend right now, totally intentional :cough:
 const checkUser = async (req, res, next) => {
   let currentUser;
   console.log('check user');
